54c588b5f113355d60c4eb84d318086dd434c043
Alex Siegel <asiegel11@student.gsu.edu>
2021-11-14 13:09:15 -0500

    First commit

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Welcome to computer science society"
+# Alex Siegel - asiegel11@student.gsu.edu
+echo -n "The current date is"
+date
+ls /home
+echo $PATH
+echo $USER
+echo $SHELL
+df
+echo "Please, could you loan me \$25.00"
+if x=1
+then
+        x='expr $x * $x'
+        x='expr $x/2'
+fi
+ls -d 'c.*(.sh)'
+echo -n "Good bye "
+date +%T
